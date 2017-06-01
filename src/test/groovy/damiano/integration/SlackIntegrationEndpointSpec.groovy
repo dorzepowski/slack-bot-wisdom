3480d90c72177c26a1a2dcccd6933963d0e03479
Damian Orzepowski <damian.orzepowski@circlekeurope.com>
2017-06-01 22:41:39 +0200

    refactoring of endpoint test

--- src/test/groovy/damiano/integration/SlackIntegrationEndpointSpec.groovy
+++ src/test/groovy/damiano/integration/SlackIntegrationEndpointSpec.groovy
@@ -7,7 +7,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ContextConfiguration
@@ -16,6 +15,7 @@ import org.springframework.util.MultiValueMap
 import spock.lang.Specification
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import static org.springframework.http.HttpMethod.GET
 
 @ContextConfiguration
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -27,28 +27,17 @@ class SlackIntegrationEndpointSpec extends Specification {
 	@Autowired
 	private TestRestTemplate restTemplate
 
-	def "Respond for command go to chanel"() {
-		given:
-			MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>()
-			map.add("token", "12345")
-			map.add("text", "very wisdom text")
 
+	def "Respond for command go to chanel"() {
 		when:
-			ResponseEntity<String> response = restTemplate.postForEntity("/", map, String.class)
-			def message = new JsonSlurper().parseText(response.body)
+			def message = send(slackCommand())
 		then:
 			message.response_type == "in_channel"
 	}
 
 	def "Respond with attachment"() {
-		given:
-			MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>()
-			map.add("token", "12345")
-			map.add("text", "very wisdom text")
-
 		when:
-			ResponseEntity<String> response = restTemplate.postForEntity("/", map, String.class)
-			def message = new JsonSlurper().parseText(response.body)
+			def message = send(slackCommand())
 		then:
 			message.attachments != null
 			message.attachments.size() == 1
@@ -56,14 +45,8 @@ class SlackIntegrationEndpointSpec extends Specification {
 	}
 
 	def "Respond with image url"() {
-		given:
-			MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>()
-			map.add("token", "12345")
-			map.add("text", "very wisdom text")
-
 		when:
-			ResponseEntity<String> response = restTemplate.postForEntity("/", map, String.class)
-			def message = new JsonSlurper().parseText(response.body)
+			def message = send(slackCommand())
 			def attachment = message.attachments.first()
 		then:
 			attachment.image_url
@@ -73,25 +56,38 @@ class SlackIntegrationEndpointSpec extends Specification {
 
 	def "Provide image for received id"() {
 		given:
-			MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>()
-			map.add("token", "12345")
-			map.add("text", "very wisdom text")
-			ResponseEntity<String> response = restTemplate.postForEntity("/", map, String.class)
-			def message = new JsonSlurper().parseText(response.body)
+			def message = send(slackCommand())
 			String imageUrl = message.attachments.first().image_url
 		when:
-			HttpHeaders headers = new HttpHeaders()
-			headers.setAccept([MediaType.IMAGE_PNG])
-			HttpEntity<String> request = new HttpEntity<String>(headers)
-
-			ResponseEntity<byte[]> entity = restTemplate.exchange(imageUrl, HttpMethod.GET, request, byte[])
-			byte[] image = entity.getBody()
-
+			byte[] image = loadImage(imageUrl)
 		then:
 			image
 			image.size() != 0
 
 	}
 
+
+	private static MultiValueMap<String, String> slackCommand() {
+		MultiValueMap<String, String> command = new LinkedMultiValueMap<String, String>()
+		command.add("token", "12345")
+		command.add("text", "very wisdom text")
+		return command
+	}
+
+	private def send(MultiValueMap<String, String> command) {
+		ResponseEntity<String> response = restTemplate.postForEntity("/", command, String.class)
+		return new JsonSlurper().parseText(response.body)
+	}
+
+	private byte[] loadImage(String imageUrl) {
+		ResponseEntity<byte[]> entity = restTemplate.exchange(imageUrl, GET, imageRequestHeaders(), byte[])
+		return entity.getBody()
+	}
+
+	private static HttpEntity<String> imageRequestHeaders() {
+		HttpHeaders headers = new HttpHeaders()
+		headers.setAccept([MediaType.IMAGE_PNG])
+		return new HttpEntity<String>(headers)
+	}
 
 }
